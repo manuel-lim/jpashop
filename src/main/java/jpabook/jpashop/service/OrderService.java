f4c37d5775ad3b85793d5bdc71f54b8038dec192
manuel.lim <manuel.lim@daumkakao.com>
2019-11-19 23:08:04 +0900

    order service 개발

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,62 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+// 주문
+// 취소
+// 검색
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    @Transactional // 데이터 변경
+    public Long order(Long memberId, Long itemId, int count) {
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    // 취소
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+    // 검색
+//
+//    public List<Order> findOrders() {
+//        return orderRepository.find
+//    }
+}
